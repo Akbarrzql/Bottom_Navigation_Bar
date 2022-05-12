a6e42f66ffef905f4980b55da9aa724ea0b54681
Akbar Rizqullah <akbarrizqullah228@gmail.com>
2022-05-12 07:10:25 +0700

    add and edit two fargment and stggered

--- app/src/main/java/com/example/bottomnavigationbar/TwoFragment.java
+++ app/src/main/java/com/example/bottomnavigationbar/TwoFragment.java
@@ -2,63 +2,57 @@ package com.example.bottomnavigationbar;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TwoFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.bottomnavigationbar.Adapter.Staggeredadapter;
+import com.example.bottomnavigationbar.Data.Pelukis;
+import com.example.bottomnavigationbar.Data.PelukisData;
+
+import java.util.ArrayList;
+
 public class TwoFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public TwoFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TwoFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TwoFragment newInstance(String param1, String param2) {
-        TwoFragment fragment = new TwoFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    RecyclerView recyclerView;
+    private ArrayList<Pelukis> list = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_two, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        recyclerView = view.findViewById(R.id.recyclerViewstragger);
+        recyclerView.setHasFixedSize(true);
+
+        recyclerView = view.findViewById(R.id.recyclerViewstragger);
+        recyclerView.setHasFixedSize(true);
+
+        list.addAll(PelukisData.getListData());
+        showStragger();
+
+        int spanCount = 2; // 2 columns
+        int spacing = 20; // 50px
+        boolean includeEdge = true;
+        recyclerView.addItemDecoration(new GridSpacingItemDecoration(spanCount, spacing, includeEdge));
+
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    private void showStragger() {
+        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL));
+        Staggeredadapter staggeredadapter = new Staggeredadapter(list);
+        recyclerView.setAdapter(staggeredadapter);
     }
 }
